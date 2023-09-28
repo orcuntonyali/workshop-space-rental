ab89c6b4cb8914f2dac8a47f0015729251c4d984
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-09-28 10:43:16 +0200

    master-cleanup

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,9 +1,14 @@
 class SpacesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  
+
   def index
     @spaces = Space.all
   end
+
   def new
   end
+
+  def show
+  end
+
 end
