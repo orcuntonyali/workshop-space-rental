e2e023ff08dc7e191e42d19a04f8252749f9d467
Shamaila Sadaf <i>
2023-09-28 10:23:36 +0200

    added show method in spaces controller

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,18 @@
 class SpacesController < ApplicationController
+  before_action :set_space, only: [:show]
 
-    def new
-    end
+  def new
+  end
+
+  def show
+  end
+
+  private
+
+  def set_space
+    @space = Space.find(params[:id])
+
+    flash[:alert] = 'Space not found'
+    redirect_to root_path
+  end
 end
