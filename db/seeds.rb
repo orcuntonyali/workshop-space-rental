faa2fd5c5e3cec624b1ca86f5923a55d1e442d7c
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-09-29 16:56:19 +0200

    important-seed-updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+# Clear previous seeds
+User.destroy_all
+Space.destroy_all
+
 # Create a few users
 users = 5.times.map do
   User.create!(
     email: Faker::Internet.unique.email,
-    password: '123456'
+    password: '123456',
+    username: Faker::Internet.unique.username,
+    role: ['booker', 'lister'].sample
   )
 end
 
-# Create spaces with more realistic and varied data
+# Create spaces with data
 20.times do
   Space.create!(
     title: "#{Faker::Educator.subject} Study Space",
