7982c623a6c89259f192e13b82edaec229a6d43d
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-09-29 16:28:36 +0200

    routes-updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "spaces#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :spaces
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :spaces do
+    resources :bookings, only: [:create] do
+      resources :reviews, only: [:new, :create]
+    end
+    resources :bookings, only: [:index]
+  end
 end
