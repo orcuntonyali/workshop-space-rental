3d67bccb7a36f499177e718e54a8063a8e199e92
Arianna Pedroni <aripedroni97@gmail.com>
2023-09-29 17:26:01 +0200

    Scaffold product model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
   root to: "spaces#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :spaces
+  resources :spaces do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
