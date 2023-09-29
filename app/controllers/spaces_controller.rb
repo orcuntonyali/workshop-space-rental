adedd084f9f8f59f6b0910b4f087ef257622b6ee
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-09-29 17:18:52 +0200

    spaces-controller-updated-for-dif-users

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,20 +1,45 @@
 class SpacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-  before_action :set_space, only: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_space, only: [:show, :edit, :update]
 
   def index
-    @spaces = Space.all
+    @spaces = current_user&.lister? ? current_user.spaces : Space.all
   end
 
   def new
+    @space = current_user.spaces.build
   end
 
   def show
+  end
+
+  def create
+    @space = current_user.spaces.build(space_params)
+    if @space.save
+      redirect_to spaces_path, notice: 'Space was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @space.update(space_params)
+      redirect_to spaces_path, notice: 'Space was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_space
     @space = Space.find(params[:id])
+  end
+
+  def space_params
+    params.require(:space).permit(:title, :description, :facilities, :equipment, :capacity, :availability, :price, images: [])
   end
 end
