2ab15df271b914e34912d0b0455352b917f92bab
Shamaila Sadaf <i>
2023-10-07 17:16:15 +0200

    added calender to search bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_102335) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_131837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_102335) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "select_date"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
