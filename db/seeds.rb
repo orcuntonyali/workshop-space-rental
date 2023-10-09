1648d61e0e0a10269be30b3d0e82fe932cd9ac3d
Orcun Tonyali <tonyaliorcun@gmail.com>
2023-10-09 16:24:34 +0200

    loco

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-require 'pry-byebug'
+
 # Clear previous seeds
 puts "Cleaning Records..."
 Space.delete_all
